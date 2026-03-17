af289ae51d54de66e18bbc8b9df59cbc3ca5b55e
Dory <dory@dory.moe>
2026-03-17 09:54:25 -0700

    simplified LED blinking

--- main/main.c
+++ main/main.c
@@ -26,7 +26,6 @@
 #include "misha_bot.h"
 #include "nvs_flash.h"
 #include "wifi_station.h"
-#include "led_strip.h"
 
 static const char* TAG = "main";
 
@@ -69,37 +68,14 @@ static void memory_monitor_task(void* pvParameter) {
 #endif
 
 static void heartbeat_led_task(void* pvParameter) {
-#ifdef CONFIG_BLINK_LED_STRIP
-  static led_strip_handle_t led_strip;
-  led_strip_config_t strip_config = {
-    .strip_gpio_num = CONFIG_LED_GPIO,
-    .max_leds = 1,
-  };
-  led_strip_rmt_config_t rmt_config = {
-      .resolution_hz = 10 * 1000 * 1000, // 10MHz
-      .flags.with_dma = false,
-  };
-  ESP_ERROR_CHECK(
-      led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip)
-  );
-  led_strip_clear(led_strip);
-  while (1) {
-    led_strip_set_pixel(led_strip, 0, 0, 4, 0);
-    led_strip_refresh(led_strip);
-    vTaskDelay(pdMS_TO_TICKS(25));
-    led_strip_clear(led_strip);
-    vTaskDelay(pdMS_TO_TICKS(2000));
-  }
-#else  // normal GPIO LED
   gpio_reset_pin(CONFIG_LED_GPIO);
   gpio_set_direction(CONFIG_LED_GPIO, GPIO_MODE_OUTPUT);
   while (1) {
     gpio_set_level(CONFIG_LED_GPIO, LED_ON);
-    vTaskDelay(pdMS_TO_TICKS(25));
+    vTaskDelay(pdMS_TO_TICKS(15));
     gpio_set_level(CONFIG_LED_GPIO, LED_OFF);
     vTaskDelay(pdMS_TO_TICKS(2000));
   }
-#endif
 }
 
 
